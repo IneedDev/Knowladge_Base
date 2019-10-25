78d02ce29b4e3ae7e27b5b641d20ae9af5d0d16a
paromaniuk <paromaniuk@inpost.pl>
2019-10-25 22:16:35 +0200

    new sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo '\n******************** STATUS ********************\n'
+git status
+echo '******************** BRANCH ********************\n'
 git branch -a
 git add .
+echo '\n'
 read -p "Commit description: " desc
+echo '\n'
 git commit -m "$desc"
-echo 'remote branch'
-read remote
+read -p "Remote branch: " remote
 echo 'your remote brach is's $remote
 git push origin $remote
