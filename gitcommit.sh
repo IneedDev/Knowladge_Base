f9e92dc56e9fb6e677fc4996ff1642fcd3202934
paromaniuk <paromaniuk@inpost.pl>
2019-10-25 22:27:09 +0200

    sh from other dir

--- gitcommit.sh
+++ gitcommit.sh
@@ -8,6 +8,8 @@ echo '\n'
 read -p "Commit description: " desc
 echo '\n'
 git commit -m "$desc"
+echo '\n'
 read -p "Remote branch: " remote
+echo '\n'
 echo 'your remote brach is's $remote
 git push origin $remote
