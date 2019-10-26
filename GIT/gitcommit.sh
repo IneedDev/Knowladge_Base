fdfe1959e7b36dea5c17e8fb5ad2e5fb83f3d9fa
IneedDev <p.romaniuk.pl@gmail.com>
2019-10-26 20:47:50 +0200

    added script with timeout

--- GIT/gitcommit.sh
+++ GIT/gitcommit.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
-
+git config --global credential.helper "cache --timeout 7200"
 files=(*/)
 counter=-1
-
+echo -e "\n \e[32m******************** PROJECTS ********************  \e[0m \n"
 for file in "${files[@]}"; do
     let counter=$counter+1
     echo "$counter - $file"
 done
-echo -e "\n \e[32m******************** PROJECT ********************  \e[0m \n"
+echo -e "\n"
 read -p "Give number of project directory: " directory
 
 cd $PWD/${files[$directory]::-1}
@@ -15,11 +15,14 @@ echo -e "\n \e[32m******************** STATUS ********************  \e[0m \n"
 git status
 echo -e "\n \e[32m******************** BRANCH ********************  \e[0m \n"
 git branch -a
+echo -e "\n \e[32m******************** GIT ADD ALL ********************  \e[0m \n"
 git add .
+git status
 echo -e "\n \e[32m******************** COMMIT ********************  \e[0m \n"
 read -p "Commit description: " desc
 git commit -m "$desc"
-read -p "Remote branch:  " remote
-echo 'your remote brach is ' $remote
 echo -e "\n \e[32m******************** PUSH ********************  \e[0m \n"
+read -p "Remote branch:  " remote
+echo 'Your remote brach is ' $remote
 git push origin $remote
+
