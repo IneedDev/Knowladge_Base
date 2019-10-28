6c25f3f3d120b323c3acc9520cac22c59edb6620
IneedDev <p.romaniuk.pl@gmail.com>
2019-10-28 08:50:28 +0100

    ini

--- src/main/java/com/streams/Stream.java
+++ src/main/java/com/streams/Stream.java
@@ -7,8 +7,6 @@ import java.util.stream.IntStream;
 public class Stream {
 
     public static void main(String[] args) {
-
-
         List<String> myList =
                 Arrays.asList("a1", "a2", "b1", "c2", "c1");
 
